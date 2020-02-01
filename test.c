d6c2490d2c11b6d5c62a482f5497c0d7454a8f7a
henryfo <60545113+henryfo@users.noreply.github.com>
2020-02-01 09:25:10 -0500

    Create test.c
    This test file exercises the MLS polynomial fitting.

---
+++ test.c
@@ -0,0 +1,13 @@
+// Name: test.c
+// Description: Test file to exercise the MLS polynomial fitting function.
+// Author: Henry Forson
+// Coyright ©2020 Henry Forson, Melbourne, Florida USA
+
+#include  <stdio.h>
+// #include  <mls.h>
+
+int main()
+{
+  printf("Hello world test stub.\n" );
+  return( 0 );
+}
